ad8a932678c4dfde09f6cd13396a074cd0c92b0e
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2016-09-19 16:38:28 +0100

    Add GruntJS with QUnit, Concat and Uglify tasks

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        qunit: {
+            all: ['tests/*.html']
+        },
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['bower_components/jquery/dist/jquery.js', 'src/app.js'],
+                dest: 'dist/app.js',
+            },
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'dist/app.js',
+                dest: 'dist/app.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['qunit', 'concat', 'uglify']);
+
+};
